556befa523d33a9e8139712912a0a841bd47e64c
inga-shvetsova <lskoryh@aol.com>
2024-05-10 12:18:24 -0400

    Create All-your-base-are-belong-to-us.c

---
+++ All-your-base-are-belong-to-us.c
@@ -0,0 +1,14 @@
+/* All-your-base-are-belong-to-us.c - I am sorry, Paul
+  Created by Inga Shvetsova, 10/05/2024 */
+
+#include <stdio>
+#define MSG1 "All your base are belong to us!"
+
+int main(void) {
+
+  const char msg[] = MSG1;
+  puts(msg);
+
+  rerurn 0;
+
+}
