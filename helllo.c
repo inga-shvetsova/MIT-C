36072da22f72305354755ef30eb9baeb49469e1c
inga-shvetsova <lskoryh@aol.com>
2024-05-10 12:10:52 -0400

    Create helllo.c

---
+++ helllo.c
@@ -0,0 +1,14 @@
+/* hello.c - My fist C program
+  Created by Inga Shvetsova, 10/05/2024 */
+
+#include <stdio.h> /* header file with basic I/O facilities */
+
+int main(void) /* void - significes no return value/arguments */
+{
+
+  const char msg[] = "Hello, kourb!"; /* string stored as character array */
+  /* stdout */
+  puts(msg);
+
+  return 0;
+}
