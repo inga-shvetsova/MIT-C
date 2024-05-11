96988d0bfafa9ac113336bc3a1c720bf7361c50e
inga-shvetsova <lskoryh@aol.com>
2024-05-10 22:06:21 -0400

    Update All-your-base-are-belong-to-us.c

--- All-your-base-are-belong-to-us.c
+++ All-your-base-are-belong-to-us.c
@@ -1,7 +1,7 @@
 /* All-your-base-are-belong-to-us.c - I am sorry, Paul
   Created by Inga Shvetsova, 10/05/2024 */
 
-#include <stdio>
+#include <stdio.h>
 #define MSG1 "All your base are belong to us!" /* Preprocessor macro */
 
 int main(void) {
@@ -12,3 +12,6 @@ int main(void) {
   rerurn 0;
 
 }
+
+/* $ gcc -Wall file-name.c -o object-code-name.o
+   $ gdb object-code-name.o */
